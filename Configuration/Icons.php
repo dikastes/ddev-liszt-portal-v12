b3a8afa776a67f2aa2f8b75e8dc65ea9049ace9a
Matthias Richter <matthias.richter@slub-dresden.de>
2024-05-29 17:13:36 +0200

    Squashed 'packages/liszt_web/' content from commit 858a39c
    git-subtree-dir: packages/liszt_web
    git-subtree-split: 858a39cbd3aebb69ba3fae578d3bebefb2d75a7e

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'site-logo' => [
+        'provider' => \Praetorius\ViteAssetCollector\IconProvider\SvgIconProvider::class,
+        'source' => 'packages/liszt_web/Resources/Private/Image/Icon/typo3.svg',
+    ],
+    'liszt-portal-logo' => [
+        'provider' => \Praetorius\ViteAssetCollector\IconProvider\SvgIconProvider::class,
+        'source' => 'packages/liszt_web/Resources/Private/Image/Icon/LogoLisztPortal.svg',
+    ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+defined('TYPO3') or die();
+
+// Include vite generated manifest file (global)
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['vite_asset_collector']['defaultManifest'] = 'EXT:liszt_web/Resources/Public/Vite/.vite/manifest.json';
+
+// Include custom RTE config
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:liszt_web/Configuration/RTE/RteDefaultPreset.yaml';
+
+
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('iconpack')) {
+    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \Quellenform\Iconpack\IconpackRegistry::class
+    )->registerIconpack(
+        'EXT:liszt_web/Configuration/Iconpack/LisztIconpack.yaml'
+    );
+}
