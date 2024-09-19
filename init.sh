34d35ef2c1fceb4c23882d589f51ffd0fb3d9601
Matthias Richter <matthias.richter@slub-dresden.de>
2024-09-19 12:49:42 +0200

    Update init script

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
- mkdir packages
- cd packages/
- git clone https://github.com/slub/liszt_web
- git clone https://github.com/slub/liszt_common
- git clone https://github.com/slub/liszt_bibliography
- git clone https://github.com/dikastes/zotero-api
- ddev typo3-init
+mkdir packages
+cd packages/
+git clone https://github.com/slub/liszt_web
+git clone https://github.com/slub/liszt_common
+git clone https://github.com/slub/liszt_bibliography
+git clone https://github.com/dikastes/zotero-api
+ddev typo3-init
+scp -r liszt-portal.de:/var/www/webroot/public/fileadmin/user_upload public/fileadmin/user_upload
+ddev import-db dev_dump.sql.gz
