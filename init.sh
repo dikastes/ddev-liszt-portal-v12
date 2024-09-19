869f25909c63c9542c5ecb930c94f05332d90a21
Matthias Richter <matthias.richter@slub-dresden.de>
2024-09-19 10:08:36 +0200

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+ mkdir packages
+ cd packages/
+ git clone https://github.com/slub/liszt_web
+ git clone https://github.com/slub/liszt_common
+ git clone https://github.com/slub/liszt_bibliography
+ git clone https://github.com/dikastes/zotero-api
+ ddev typo3-init
