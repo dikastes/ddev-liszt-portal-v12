cd0855f2e0ef1c0346ddec87f70401e3a03e8161
Matthias Richter <matthias.richter@slub-dresden.de>
2024-09-19 13:44:31 +0200

    Add init features

--- init.sh
+++ init.sh
@@ -1,9 +1,31 @@
-mkdir packages
+#!/usr/bin/env bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "Initializing the Liszt Portal development environment"
+echo "====================================================="
+echo "${bold}1. Cloning the repositories${normal}"
+echo "2. Setting up the TYPO3 environment"
+echo "3. Importing the database"
+
+[ ! -d packages ] && mkdir packages
 cd packages/
-git clone https://github.com/slub/liszt_web
-git clone https://github.com/slub/liszt_common
-git clone https://github.com/slub/liszt_bibliography
-git clone https://github.com/dikastes/zotero-api
+[ ! -d liszt_web ] && git clone https://github.com/slub/liszt_web
+[ ! -d liszt_common ] && git clone https://github.com/slub/liszt_common
+[ ! -d liszt_bibliography ] && git clone https://github.com/slub/liszt_bibliography
+[ ! -d zotero-api ] && git clone https://github.com/dikastes/zotero-api
+cd ..
+
+echo "1. Cloning the repositories"
+echo "${bold}2. Setting up the TYPO3 environment${normal}"
+echo "3. Importing the database"
+
 ddev typo3-init
-scp -r liszt-portal.de:/var/www/webroot/public/fileadmin/user_upload public/fileadmin/user_upload
+
+echo "1. Cloning the repositories"
+echo "2. Setting up the TYPO3 environment"
+echo "${bold}3. Importing the database${normal}"
+
 ddev import-db dev_dump.sql.gz
+scp -r test.liszt-portal.de:/var/www/webroot/public/fileadmin/user_upload public/fileadmin/user_upload
