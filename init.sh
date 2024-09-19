4585b844ac4d48649ecb47427152821df57c7b11
Matthias Richter <matthias.richter@slub-dresden.de>
2024-09-19 14:08:39 +0200

    Add cache flush

--- init.sh
+++ init.sh
@@ -7,7 +7,8 @@ echo "Initializing the Liszt Portal development environment"
 echo "====================================================="
 echo "${bold}1. Cloning the repositories${normal}"
 echo "2. Setting up the TYPO3 environment"
-echo "3. Importing the database"
+echo "3. Importing database and files"
+echo "4. Cache flush"
 
 [ ! -d packages ] && mkdir packages
 cd packages/
@@ -19,13 +20,22 @@ cd ..
 
 echo "1. Cloning the repositories"
 echo "${bold}2. Setting up the TYPO3 environment${normal}"
-echo "3. Importing the database"
+echo "3. Importing database and files"
+echo "4. Cache flush"
 
 ddev typo3-init
 
 echo "1. Cloning the repositories"
 echo "2. Setting up the TYPO3 environment"
-echo "${bold}3. Importing the database${normal}"
+echo "${bold}3. Importing database and files${normal}"
+echo "4. Cache flush"
 
-ddev import-db dev_dump.sql.gz
+ddev import-db --file=dev_dump.sql.gz
 scp -r test.liszt-portal.de:/var/www/webroot/public/fileadmin/user_upload public/fileadmin/user_upload
+
+echo "1. Cloning the repositories"
+echo "2. Setting up the TYPO3 environment"
+echo "3. Importing database and files"
+echo "${bold}4. Cache flush${normal}"
+
+ddev typo3 cache:flush
